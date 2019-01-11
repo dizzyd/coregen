2218c2d08204f1b46bcae41f4a445911cf5b5307
Dave Smith <dizzyd@dizzyd.com>
2019-01-10 20:22:22 -0700

    Add Position wrapper object for BlockPos

---
+++ src/main/java/com/dizzyd/coregen/scripting/Position.java
@@ -0,0 +1,43 @@
+// ***************************************************************************
+//
+//  Copyright 2018-2019 David (Dizzy) Smith, dizzyd@dizzyd.com
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// ***************************************************************************
+package com.dizzyd.coregen.scripting;
+
+import net.minecraft.util.math.BlockPos;
+
+public class Position {
+    private BlockPos pos;
+
+    public Position(int x, int y, int z) {
+        pos = new BlockPos(x, y, z);
+    }
+
+    public Position(double x, double y, double z) {
+        pos = new BlockPos(x, y, z);
+    }
+
+    public int getX() {
+        return pos.getX();
+    }
+
+    public int getY() {
+        return pos.getY();
+    }
+
+    public int getZ() {
+        return pos.getZ();
+    }
+}
